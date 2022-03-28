f20834019c078cd276e57b5036349edbbd866f17
Merzouk-ilyes <i.merzouk@esi-sba.dz>
2022-03-28 10:48:28 +0100

    setting up dependencies

---
+++ src/components/host/Host.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Host() {
+  return (
+    <div>Host</div>
+  )
+}
+
+export default Host
--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Admin from "./components/admin/Admin";
+import Agent from "./components/agent/Agent";
+import Host from "./components/host/Host";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="admin" element={<Admin />} />
+      <Route path="host" element={<Host />} />
+      <Route path="agent" element={<Agent />} />
+
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: "3rem" }}>
+            <p>There's nothing here , Leave me alone !</p>
+          </main>
+        }
+      />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
