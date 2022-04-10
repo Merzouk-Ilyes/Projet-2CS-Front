d65d584d1446f881884a080527368ee527265bf7
med-zino <medzino85@gmail.com>
2022-04-10 00:32:57 +0000

    handling a pull conflict

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
@@ -11,67 +10,52 @@ import SignupHost from './components/auth/signup_host'
 import SignupClient from './components/auth/signup_client'
 import { ChakraProvider } from '@chakra-ui/react'
 import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import ResetPwd from './components/auth/reset'
-import ForgetPassword from './components/auth/forget'
-=======
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Admin from "./components/admin/Admin";
-import Agent from "./components/agent/Agent";
-import Host from "./components/host/Host";
-import Login from "./components/auth/login";
-import SignupHost from "./components/auth/signup_host";
-import SignupClient from "./components/auth/signup_client";
-import { ChakraProvider } from "@chakra-ui/react";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import ResetPwd from "./components/auth/reset";
 import { store } from './state/store'
 import { Provider } from 'react-redux'
->>>>>>> 8c571e8ef0fea5fd72a39a011db030fddbe4847e
+import ForgetPassword from './components/auth/forget'
 
 ReactDOM.render(
   <Provider store={store}>
-  <ChakraProvider>
-    <ToastContainer
-      position="top-center"
-      autoClose={3500}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-    />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signuphost" element={<SignupHost />} />
-        <Route path="/signupclient" element={<SignupClient />} />
-        <Route path="/reset" element={<ResetPwd />} />
-        <Route path="/forget" element={<ForgetPassword />} />
-        <Route path="admin" element={<Admin />} />
-        <Route path="host" element={<Host />} />
-        <Route path="agent" element={<Agent />} />
+    <ChakraProvider>
+      <ToastContainer
+        position='top-center'
+        autoClose={3500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signuphost' element={<SignupHost />} />
+          <Route path='/signupclient' element={<SignupClient />} />
+          <Route path='/reset' element={<ResetPwd />} />
+          <Route path='/forget' element={<ForgetPassword />} />
+          <Route path='admin' element={<Admin />} />
+          <Route path='host' element={<Host />} />
+          <Route path='agent' element={<Agent />} />
 
-        <Route
-          path='*'
-          element={
-            <div style={{ margin: "45vh 45vw" }}>
-              <h2>404 | NOT FOUND</h2>
-            </div>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  </ChakraProvider>
+          <Route
+            path='*'
+            element={
+              <div style={{ margin: '45vh 45vw' }}>
+                <h2>404 | NOT FOUND</h2>
+              </div>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
   </Provider>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
