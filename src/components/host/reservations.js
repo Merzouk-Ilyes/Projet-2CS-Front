0513d609bc193ec76ab673b4d2ad9dc6dd7fca15
Merzouk-ilyes <i.merzouk@esi-sba.dz>
2022-05-13 15:49:22 +0100

    host reservation table layout

--- src/components/host/reservations.js
+++ src/components/host/reservations.js
@@ -1,247 +1,58 @@
 import React from "react";
 import SidebarWithHeader from "./layout";
-
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  IconButton,Avatar
+} from "@chakra-ui/react";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { AiOutlineDelete, AiOutlineThunderbolt } from "react-icons/ai";
+import { BiDetail } from "react-icons/bi";
 function Reservations() {
   return (
     <SidebarWithHeader>
-      <div class="container mx-auto px-4 sm:px-8">
-        <div class="py-8">
-          <div>
-            <h2 class="text-2xl font-semibold leading-tight">Invoices</h2>
-          </div>
-          <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-            <div class="inline-block min-w-full shadow-md rounded-lg overflow-hidden">
-              <table class="min-w-full leading-normal">
+      <div className="container mx-auto px-4 sm:px-8">
+        <div className="py-8">
+          <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+            <div className="inline-block min-w-full shadow-md rounded-lg overflow-hidden">
+              <table className="min-w-full leading-normal">
                 <thead>
                   <tr>
-                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                      Client / Invoice
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                      Client
                     </th>
-                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
                       Amount
                     </th>
-                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                      Issued / Due
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                      Start date
                     </th>
-                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                      End date
+                    </th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
                       Status
                     </th>
-                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100"></th>
+                    <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100"></th>
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <div class="flex">
-                        <div class="flex-shrink-0 w-10 h-10">
-                          <img
-                            class="w-full h-full rounded-full"
-                            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
-                            alt=""
-                          />
-                        </div>
-                        <div class="ml-3">
-                          <p class="text-gray-900 whitespace-no-wrap">
-                            Molly Sanders
-                          </p>
-                          <p class="text-gray-600 whitespace-no-wrap">000004</p>
-                        </div>
-                      </div>
-                    </td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p class="text-gray-900 whitespace-no-wrap">$20,000</p>
-                      <p class="text-gray-600 whitespace-no-wrap">USD</p>
-                    </td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p class="text-gray-900 whitespace-no-wrap">
-                        Sept 28, 2019
-                      </p>
-                      <p class="text-gray-600 whitespace-no-wrap">
-                        Due in 3 days
-                      </p>
-                    </td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <span class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-                        <span
-                          aria-hidden
-                          class="absolute inset-0 bg-green-200 opacity-50 rounded-full"
-                        ></span>
-                        <span class="relative">Paid</span>
-                      </span>
-                    </td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right">
-                      <button
-                        type="button"
-                        class="inline-block text-gray-500 hover:text-gray-700"
-                      >
-                        <svg
-                          class="inline-block h-6 w-6 fill-current"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M12 6a2 2 0 110-4 2 2 0 010 4zm0 8a2 2 0 110-4 2 2 0 010 4zm-2 6a2 2 0 104 0 2 2 0 00-4 0z" />
-                        </svg>
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <div class="flex">
-                        <div class="flex-shrink-0 w-10 h-10">
-                          <img
-                            class="w-full h-full rounded-full"
-                            src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
-                            alt=""
-                          />
-                        </div>
-                        <div class="ml-3">
-                          <p class="text-gray-900 whitespace-no-wrap">
-                            Michael Roberts
-                          </p>
-                          <p class="text-gray-600 whitespace-no-wrap">000003</p>
-                        </div>
-                      </div>
-                    </td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p class="text-gray-900 whitespace-no-wrap">$214,000</p>
-                      <p class="text-gray-600 whitespace-no-wrap">USD</p>
-                    </td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p class="text-gray-900 whitespace-no-wrap">
-                        Sept 25, 2019
-                      </p>
-                      <p class="text-gray-600 whitespace-no-wrap">
-                        Due in 6 days
-                      </p>
-                    </td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <span class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-                        <span
-                          aria-hidden
-                          class="absolute inset-0 bg-green-200 opacity-50 rounded-full"
-                        ></span>
-                        <span class="relative">Paid</span>
-                      </span>
-                    </td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right">
-                      <button
-                        type="button"
-                        class="inline-block text-gray-500 hover:text-gray-700"
-                      >
-                        <svg
-                          class="inline-block h-6 w-6 fill-current"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M12 6a2 2 0 110-4 2 2 0 010 4zm0 8a2 2 0 110-4 2 2 0 010 4zm-2 6a2 2 0 104 0 2 2 0 00-4 0z" />
-                        </svg>
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <div class="flex">
-                        <div class="flex-shrink-0 w-10 h-10">
-                          <img
-                            class="w-full h-full rounded-full"
-                            src="https://images.unsplash.com/photo-1540845511934-7721dd7adec3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
-                            alt=""
-                          />
-                        </div>
-                        <div class="ml-3">
-                          <p class="text-gray-900 whitespace-no-wrap">
-                            Devin Childs
-                          </p>
-                          <p class="text-gray-600 whitespace-no-wrap">000002</p>
-                        </div>
-                      </div>
-                    </td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p class="text-gray-900 whitespace-no-wrap">$20,000</p>
-                      <p class="text-gray-600 whitespace-no-wrap">USD</p>
-                    </td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p class="text-gray-900 whitespace-no-wrap">
-                        Sept 14, 2019
-                      </p>
-                      <p class="text-gray-600 whitespace-no-wrap">
-                        Due in 2 weeks
-                      </p>
-                    </td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <span class="relative inline-block px-3 py-1 font-semibold text-orange-900 leading-tight">
-                        <span
-                          aria-hidden
-                          class="absolute inset-0 bg-orange-200 opacity-50 rounded-full"
-                        ></span>
-                        <span class="relative">Pending</span>
-                      </span>
-                    </td>
-                    <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right">
-                      <button
-                        type="button"
-                        class="inline-block text-gray-500 hover:text-gray-700"
-                      >
-                        <svg
-                          class="inline-block h-6 w-6 fill-current"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M12 6a2 2 0 110-4 2 2 0 010 4zm0 8a2 2 0 110-4 2 2 0 010 4zm-2 6a2 2 0 104 0 2 2 0 00-4 0z" />
-                        </svg>
-                      </button>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td class="px-5 py-5 bg-white text-sm">
-                      <div class="flex">
-                        <div class="flex-shrink-0 w-10 h-10">
-                          <img
-                            class="w-full h-full rounded-full"
-                            src="https://images.unsplash.com/photo-1522609925277-66fea332c575?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&h=160&w=160&q=80"
-                            alt=""
-                          />
-                        </div>
-                        <div class="ml-3">
-                          <p class="text-gray-900 whitespace-no-wrap">
-                            Frederick Nicholas
-                          </p>
-                          <p class="text-gray-600 whitespace-no-wrap">000001</p>
-                        </div>
-                      </div>
-                    </td>
-                    <td class="px-5 py-5 bg-white text-sm">
-                      <p class="text-gray-900 whitespace-no-wrap">$12,000</p>
-                      <p class="text-gray-600 whitespace-no-wrap">USD</p>
-                    </td>
-                    <td class="px-5 py-5 bg-white text-sm">
-                      <p class="text-gray-900 whitespace-no-wrap">
-                        Sept 6, 2019
-                      </p>
-                      <p class="text-gray-600 whitespace-no-wrap">
-                        Due 3 weeks ago
-                      </p>
-                    </td>
-                    <td class="px-5 py-5 bg-white text-sm">
-                      <span class="relative inline-block px-3 py-1 font-semibold text-red-900 leading-tight">
-                        <span
-                          aria-hidden
-                          class="absolute inset-0 bg-red-200 opacity-50 rounded-full"
-                        ></span>
-                        <span class="relative">Overdue</span>
-                      </span>
-                    </td>
-                    <td class="px-5 py-5 bg-white text-sm text-right">
-                      <button
-                        type="button"
-                        class="inline-block text-gray-500 hover:text-gray-700"
-                      >
-                        <svg
-                          class="inline-block h-6 w-6 fill-current"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M12 6a2 2 0 110-4 2 2 0 010 4zm0 8a2 2 0 110-4 2 2 0 010 4zm-2 6a2 2 0 104 0 2 2 0 00-4 0z" />
-                        </svg>
-                      </button>
-                    </td>
-                  </tr>
+                  <TableItem
+                    clientName="Merzouk ilyes reda"
+                    amount="100"
+                    startDate="may 13,2022"
+                    endDate="may 20,2022"
+                    status="Pending"
+                  />
+                  <TableItem
+                    clientName="Abir benaissa"
+                    amount="100"
+                    startDate="may 13,2022"
+                    endDate="may 20,2022"
+                    status="Refused"
+                  />
                 </tbody>
               </table>
             </div>
@@ -253,3 +64,85 @@ function Reservations() {
 }
 
 export default Reservations;
+
+const TableItem = ({ clientName, amount, startDate, endDate, status }) => {
+  return (
+    <tr>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <div className="flex">
+          <div className="flex-shrink-0 w-10 h-10 flex items-center">
+          <Avatar name={clientName} size="sm" />
+          </div>
+          <div className="ml-3 flex items-center">
+            <p className="text-gray-900 whitespace-no-wrap">{clientName}</p>
+          </div>
+        </div>
+      </td>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-900 whitespace-no-wrap">{amount}$</p>
+      </td>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-900 whitespace-no-wrap">{startDate}</p>
+        <p className="text-gray-600 whitespace-no-wrap">Due in 3 days</p>
+      </td>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-900 whitespace-no-wrap">{endDate}</p>
+        <p className="text-gray-600 whitespace-no-wrap">Due in 3 days</p>
+      </td>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <Status status={status} />
+      </td>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right">
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            aria-label="Options"
+            icon={<BsThreeDotsVertical />}
+            variant="outline"
+            fontSize={20}
+          />
+          <MenuList>
+            <MenuItem icon={<BiDetail fontSize={20} />} fontSize={18}>
+              Details
+            </MenuItem>
+            <MenuItem icon={<AiOutlineThunderbolt fontSize={20}/>} fontSize={18}>
+              Take action
+            </MenuItem>
+            <MenuItem icon={<AiOutlineDelete fontSize={20}/>} fontSize={18}>
+              Delete
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </td>
+    </tr>
+  );
+};
+
+const Status = ({ status }) => {
+  var bgColor;
+  var color;
+  if (status.toLowerCase().includes("acc")) {
+    bgColor = "bg-green-200";
+    color = "text-green-900";
+  } else if (status.toLowerCase().includes("ref")) {
+    bgColor = "bg-red-200";
+    color = "text-red-900";
+  } else if (status.toLowerCase().includes("on")) {
+    bgColor = "bg-blue-200";
+    color = "text-blue-900";
+  } else if (status.toLowerCase().includes("pen")) {
+    bgColor = "bg-orange-200";
+    color = "text-orange-900";
+  }
+  return (
+    <span
+      className={`relative inline-block px-3 py-1 font-semibold ${color} leading-tight`}
+    >
+      <span
+        aria-hidden
+        className={`absolute inset-0  ${bgColor} opacity-50 rounded-full`}
+      ></span>
+      <span className="relative">{status}</span>
+    </span>
+  );
+};
